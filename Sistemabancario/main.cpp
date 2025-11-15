0e633f19a2f31829405dd2b53c50089cff592fd0
ORMatheus <Matheus.rdeolivera77@gmail.com>
2025-11-15 17:19:03 -0300

    Atualização da função helper

--- Sistemabancario/main.cpp
+++ Sistemabancario/main.cpp
@@ -9,6 +9,20 @@
 //usar o std::vector para armazenar os dados
 std::vector<Conta> contas;
 
+void exibirMenu() {
+    std::cout << "\n--- Sistema de Gerenciamento Bancário ---" << std::endl;
+    std::cout << "1. Criar nova conta" << std::endl;
+    std::cout << "2. Fazer depósito" << std::endl;
+    std::cout << "3. Fazer saque" << std::endl;
+    std::cout << "4. Consultar saldo/detalhes" << std::endl;
+    std::cout << "5. Listar todas as contas" << std::endl;
+    std::cout << "0. Sair do sistema" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+    std::cout << "Escolha uma opção: ";
+}
+
+
+
 
 int main(){
     std::cout << "=== Bem-vindo ao Sistema Bancário (Teste de Classe) ===" << std::endl;
