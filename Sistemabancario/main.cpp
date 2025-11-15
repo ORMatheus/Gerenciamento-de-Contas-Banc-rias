9bd1802f73bc96219e85e1b87c4234b0868b9d9f
ORMatheus <Matheus.rdeolivera77@gmail.com>
2025-11-14 22:05:23 -0300

    Definição de um metodo auxiliar para visualização mais organizada dos dados

--- Sistemabancario/main.cpp
+++ Sistemabancario/main.cpp
@@ -1,3 +1,13 @@
 #include "Conta.h"
 #include <iostream>
 #include <iomanip>
+using namespace std;
+//função auxiliar 
+
+void exibirDetalhes(const Conta& conta){
+    cout << "----------------------------------------" << std::endl;
+    cout << "Titular: \t " << conta.getNomeTitular() << endl;
+    cout << "Conta N.:\t" << conta.getNumeroConta()<< endl;
+    cout << "Saldo atual da sua conta:\t" << "R$ " << fixed<< setprecision(2) << conta.getSaldo() << endl;
+    cout << "----------------------------------------" << std::endl;    
+}
