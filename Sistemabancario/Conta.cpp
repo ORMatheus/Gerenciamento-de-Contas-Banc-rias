4ee30c02c6db6fe3c7ba8f93f790908e12425d0e
ORMatheus <Matheus.rdeolivera77@gmail.com>
2025-11-14 21:42:25 -0300

    Defição do metodo de saque

--- Sistemabancario/Conta.cpp
+++ Sistemabancario/Conta.cpp
@@ -11,7 +11,7 @@ Conta::Conta(int numero, std::string nome, double depositoInicial){
         saldo=depositoInicial;
     }else{
         saldo =0.0;
-        std::cout << "Deposito Negativo ignorado.\nSaldo inicial da conta esta em 0."
+        std::cout << "Deposito Negativo ignorado.\nSaldo inicial da conta esta em 0.";
     }
 
 
@@ -25,4 +25,22 @@ void Conta::depositar(double valor){
     }else{
         std::cout << "ERRO: Valor de depósito invalido, deposite um valor posito." << std::endl;
     }
+}
+
+bool Conta::sacar(double valor){
+    if(valor <=0){
+        std::cout <<"ERRO:Para realizar um saque o valor precisa ser positivo."<<std::endl;
+        return false;
+    }
+    if(valor > saldo){
+        std::cout << "ERRO: Saldo insuficiente ." << std::endl;
+        std::cout << "Seu saldo atual : R$ "<< std::fixed << std::setprecision(2) << getSaldo() << std::endl;
+        return false;
+    }
+    saldo=saldo-valor;
+    std::cout << "Saque no Valor de R$" <<std::fixed << std::setprecision(2) << valor 
+    <<"Realizado com sucesso." << std::endl;
+    std::cout <<"Seu saldo Atual esta no valor de R$"
+    << std::fixed << std::setprecision(2) << getSaldo() << std::endl;
+    return true;
 }
