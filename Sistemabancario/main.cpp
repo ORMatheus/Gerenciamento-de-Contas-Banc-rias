b7a233704c2a15bd5c904c09e11737205f77247f
ORMatheus <Matheus.rdeolivera77@gmail.com>
2025-11-14 22:24:18 -0300

    Teste 1 para ver o funcionamento

--- Sistemabancario/main.cpp
+++ Sistemabancario/main.cpp
@@ -11,3 +11,15 @@ void exibirDetalhes(const Conta& conta){
     cout << "Saldo atual da sua conta:\t" << "R$ " << fixed<< setprecision(2) << conta.getSaldo() << endl;
     cout << "----------------------------------------" << std::endl;    
 }
+
+int main(){
+    cout << "Seja Bem Vindo Ao Sistema Bancário ===" << endl;
+
+    //teste 1 criação de conta.
+    cout << "\n Criando Conta para 'Fernando ' (N.1001) Com deposito Inicial de R$ 500.00\n";
+    Conta conta1(1001,"Fernando", 500.00);
+    exibirDetalhes(conta1);
+
+    //teste 2
+    return 0;
+}
