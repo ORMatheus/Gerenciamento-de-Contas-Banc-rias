ebe176b7c97f5a0f42406d81b45d31fcb8da2046
ORMatheus <Matheus.rdeolivera77@gmail.com>
2025-11-15 17:15:24 -0300

    Correção da main.cpp

--- Sistemabancario/main.cpp
+++ Sistemabancario/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <iomanip>
-#include "Conta.h"
+#include <iomanip>    // Para std::fixed, std::setprecision
+#include <string>
+#include <vector>     // Para armazenar a lista de contas
+#include <limits>     // Para limpar o buffer de entrada (std::numeric_limits)
 
-void exibirDetalhes(){
-    
-}
+#include "Conta.h"    // Inclui nossa classe
 
 
 int main(){
+    std::cout << "=== Bem-vindo ao Sistema Bancário (Teste de Classe) ===" << std::endl;
 
     return 0;
 }
