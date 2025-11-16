83cd07c5ed58798d9b241d01cfa9b38b087ff80e
Matheus <Matheus.rdeoliveira77@gmail.com>
2025-11-16 18:04:26 -0300

    correção

--- Sistemabancario/main.cpp
+++ Sistemabancario/main.cpp
@@ -45,14 +45,14 @@ void criarConta(){
 
     //logica de validação do numero fornecido 
     if(std::cin.fail()){
-        std::cout << "Erro: número da conta invalida." <<std::endl;
+        std::cout << "Erro: numero da conta invalida." <<std::endl;
         std::cin.clear();//limpar o buffer do cin
         limparBufferEntrada();
         return;
     }
     //validação caso a conta ja exista
     if(encontrarConta(numero) !=nullptr){ //caso a conta ja exista ele vai encontrar uma posição no vector de contas 
-        std::cout << "Erro:já Exsite uma conta com esse número ." << std:: endl;
+        std::cout << "Erro:já Exsite uma conta com esse numero ." << std:: endl;
         limparBufferEntrada();
         return;
     }
@@ -61,7 +61,7 @@ void criarConta(){
     limparBufferEntrada() ; //limpar o enter deixado pelo cin anterior do >>numero
     std::getline(std::cin,nome);
 
-    std::cout << "Digite o deposito inicial (0 se não houver) :";
+    std::cout << "Digite o deposito inicial (0 se nao houver) :";
     std::cin >> depositoInicial;
 
     if(std::cin.fail()){
@@ -84,11 +84,11 @@ void fazerDeposito(){
     int numero;
     double valor;
     std::cout <<"\n--- Fazer Deposito ---"<< std::endl;
-    std::cout << "Digite o número da conta:";
+    std::cout << "Digite o numero da conta:";
     std::cin >> numero;
     Conta*conta=encontrarConta(numero);
     if(conta==nullptr){
-        std::cout << "Erro:Conta não encontrada." <<std::endl;
+        std::cout << "Erro:Conta nao encontrada." <<std::endl;
     }else {
         std::cout << "Valor a ser depositado:"<< valor;
         if(std::cin.fail()){
@@ -105,11 +105,11 @@ void fazerSaque(){
     int numero;
     double valor;
     std::cout << "\nFazer Saque---";
-    std::cout << "Digite o número da sua conta:";
+    std::cout << "Digite o numero da sua conta:";
     std::cin >>numero;
     Conta* conta=encontrarConta(numero);
     if(conta==nullptr){
-        std::cout<< "Erro:conta não encontrada." << std::endl;
+        std::cout<< "Erro:conta nao encontrada." << std::endl;
     }else {
         std::cout << "Digite o valor a sacar :";
         std::cin >> valor;
@@ -126,12 +126,12 @@ void fazerSaque(){
 void consultarSaldo(){
    int numero;
     std::cout << "\n--- Consultar Saldo ---" << std::endl;
-    std::cout << "Digite o número da conta: ";
+    std::cout << "Digite o numero da conta: ";
     std::cin >> numero;
 
     Conta* conta=encontrarConta(numero);
     if(conta==nullptr){
-        std::cout <<"Erro:Conta não encontrada." << std::endl;
+        std::cout <<"Erro:Conta nao encontrada." << std::endl;
     }else{
         std::cout << "\n--- Detalhes da conta ---"<<std::endl;
         std::cout <<"Titular" <<conta->getNomeTitular()<<std::endl;
